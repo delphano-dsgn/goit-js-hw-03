24a80a01553d2b2d9ea84bd4ece2af60c193dae5
Anastasiia Pototska <delphano86@gmail.com>
2026-02-16 21:24:08 +0100

    hw 3

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const slugify = function (title) {
+    const lowerTitle = title.toLowerCase();
+    const splitTitle = lowerTitle.split(' ');
+    const joinTitle = splitTitle.join('-');
+    return joinTitle;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
